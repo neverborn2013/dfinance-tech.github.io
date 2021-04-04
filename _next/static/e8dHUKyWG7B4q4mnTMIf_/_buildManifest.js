f322fcff5b281dea59ca69d3c4336bc5f751a438
W.H <amoslover@163.com>
2021-04-04 19:08:43 +0800

    add font files

---
+++ _next/static/e8dHUKyWG7B4q4mnTMIf_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fd750c2add935aea91ef9.css","static\u002Fchunks\u002Fpages\u002Findex-61cea9c573a5813d31cf.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3203e7e37e39af6cd7ee.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
