92d995432e1f7c95abe184805fdc4cedbe96138c
amoslover <amoslover@163.com>
2021-04-13 19:05:21 +0800

    fixed

---
+++ _next/static/6VByLdjNKlx2hxj9wP_Y7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fca8cfe70b11d71ca244e.css","static\u002Fchunks\u002Fpages\u002Findex-c4c3d47df88de6b2c4b7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3203e7e37e39af6cd7ee.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
