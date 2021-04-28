e86639427cf19b77dd7a63f2cc9911219e02e403
AmberHu <amoslover@163.com>
2021-04-28 10:02:37 +0800

    update

---
+++ _next/static/webpack/pages/_app.fc0480fc541c021375a4.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate_N_E("pages/_app",{
+
+/***/ "./public/img/dtoken.svg":
+/*!*******************************!*\
+  !*** ./public/img/dtoken.svg ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "static/media/dtoken.a0acf3b4eb60d61672848697612ceb63.svg");
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcHVibGljL2ltZy9kdG9rZW4uc3ZnIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQTtBQUFlLG9GQUF1Qiw2REFBNkQsRSIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9wYWdlcy9fYXBwLmZjMDQ4MGZjNTQxYzAyMTM3NWE0LmhvdC11cGRhdGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBfX3dlYnBhY2tfcHVibGljX3BhdGhfXyArIFwic3RhdGljL21lZGlhL2R0b2tlbi5hMGFjZjNiNGViNjBkNjE2NzI4NDg2OTc2MTJjZWI2My5zdmdcIjsiXSwic291cmNlUm9vdCI6IiJ9
